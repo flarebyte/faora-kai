87c268a139c0649ae674804cbaf346b24b79eb39
Olivier Huin <olih@users.noreply.github.com>
2023-05-20 07:00:53 +0100

    Adds basic test

---
+++ test/parsing-utils.test.ts
@@ -0,0 +1,25 @@
+import {test} from 'node:test';
+import assert from 'node:assert';
+import {z} from 'zod';
+import {stringFields} from '../src/primitive-fields.js';
+import {safeParse} from '../src/parsing-utils.js';
+
+const schema = z.object({
+  name: stringFields.string1To10,
+  tags: z.array(stringFields.string1To20).min(1),
+});
+
+type TestSchema = z.infer<typeof schema>;
+
+test('safeParse should parse correct data', () => {
+  const content = {
+    name: 'some-name',
+    tags: ['tag1'],
+  };
+  const result = safeParse<TestSchema>(content, {
+    schema,
+    formatting: 'standard',
+  });
+  assert.equal(result.status, 'success');
+  assert.deepEqual(result.status === 'success' && result.value, content);
+});
