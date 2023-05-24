50410b346740df4af787a8ed65a987ff8b65817d
Olivier Huin <olih@users.noreply.github.com>
2023-05-24 09:07:32 +0100

    Adds string too small and too big

--- test/parsing-utils.test.ts
+++ test/parsing-utils.test.ts
@@ -8,14 +8,18 @@ import {assertSuccessfulResult, assertFailedResult} from './assert-utils.js';
 const schema = z.object({
   name: stringFields.string1To10,
   tags: z.array(stringFields.string1To20).min(1),
+  website: stringFields.string1To50.url(),
 });
 
 type TestSchema = z.infer<typeof schema>;
+
+const largeString = (count: number) => 'a'.repeat(count);
 
 test('safeParse should parse correct data', () => {
   const content = {
     name: 'some-tag',
     tags: ['tag1'],
+    website: 'https://website.com',
   };
   const result = safeParse<TestSchema>(content, {
     schema,
@@ -24,10 +28,11 @@ test('safeParse should parse correct data', () => {
   assertSuccessfulResult(result, content);
 });
 
-test('safeParse should reject invalid data', () => {
+test('safeParse should reject string too small', () => {
   const content = {
     name: '',
     tags: ['tag1'],
+    website: 'https://website.com',
   };
   const result = safeParse<TestSchema>(content, {
     schema,
@@ -38,6 +43,25 @@ test('safeParse should reject invalid data', () => {
       path: 'name',
       message:
         'The string for the field is too small; I would expect the minimum to be 1',
+    },
+  ]);
+});
+
+test('safeParse should reject string too big', () => {
+  const content = {
+    name: largeString(100),
+    tags: ['tag1'],
+    website: 'https://website.com',
+  };
+  const result = safeParse<TestSchema>(content, {
+    schema,
+    formatting: 'standard',
+  });
+  assertFailedResult(result, [
+    {
+      path: 'name',
+      message:
+        'The string for the field is too big; I would expect the maximum to be 10',
     },
   ]);
 });
