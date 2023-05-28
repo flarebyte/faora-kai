29edeef13da4dd7c275573db65e8b45c40029d2c
Olivier Huin <olih@users.noreply.github.com>
2023-05-28 09:11:53 +0100

    Perhaps we don't need to export the types

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,3 @@ export {describeEnum} from './enum-utils.js';
 export {isParsingSuccessful, safeParse} from './parsing-utils.js';
 
 export {stringEffectFields, stringFields} from './primitive-fields.js';
-
-export {
-  type StringEffectFieldValidator,
-  type StringFieldValidator,
-} from './model.js';
